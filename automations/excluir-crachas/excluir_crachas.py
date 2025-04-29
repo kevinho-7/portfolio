5b7ba9b4ebf963051b8ef73bf1dee3a93304da50
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-04-29 16:49:53 -0300

    yep

--- automations/excluir-crachas/excluir_crachas.py
+++ automations/excluir-crachas/excluir_crachas.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import sync_playwright
-import funcs
+import funcs as f
 
 with sync_playwright() as p:
 
@@ -9,14 +9,19 @@ with sync_playwright() as p:
     pag.goto("https://www.ucsexplorer.com.br/login/login.aspx")
     print("Abriu zé\n")
 
+    # logar no UCS Explorer
     pag.locator("#ContentPlaceHolder1_txtemail").wait_for()
     pag.locator("#ContentPlaceHolder1_txtemail").fill("suporte@santanna.g12.br")
 
     pag.locator("#ContentPlaceHolder1_txtsenha").wait_for()
-    pag.locator("#ContentPlaceHolder1_txtsenha").fill(funcs.get_senha())
+    pag.locator("#ContentPlaceHolder1_txtsenha").fill(f.get_senha())
 
+    pag.locator("#ContentPlaceHolder1_cmdLogin").wait_for()
+    pag.locator("#ContentPlaceHolder1_cmdLogin").click()
 
-
+    # procurar usuario
+    pag.locator("a[title='Usuário']").wait_for()
+    pag.locator("a[title='Usuário']").click()
 
 
 
--- automations/excluir-crachas/funcs.py
+++ automations/excluir-crachas/funcs.py
@@ -1,4 +1,6 @@
+import excluir_crachas as e
 
 def get_senha():
     password = "!Noceutempao1043"
     return password
+
