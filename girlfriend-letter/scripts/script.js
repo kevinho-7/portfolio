65476453cc93292a57af6e0bcaf7f612e0f6fab8
KevinFerreira-BYUI <kferreira@byupathway.edu>
2025-06-05 16:13:40 -0300

    yeeaaah bro

--- girlfriend-letter/scripts/script.js
+++ girlfriend-letter/scripts/script.js
@@ -9,3 +9,26 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 elements.forEach(el => observer.observe(el));
+
+document.addEventListener("DOMContentLoaded", () => {
+  const clickSong = document.getElementById("btn1");
+  const song = document.getElementById("song");
+  const slide1 = document.getElementById("slide1");
+
+  clickSong.addEventListener("click", () => {
+    song.play();
+    slide1.scrollIntoView();
+  });
+
+  window.onload = function () {
+    const loader = document.querySelector(".loader");
+
+    setTimeout(() => {
+      loader.classList.add("hide");
+
+      setTimeout(() => {
+        loader.style.display = "none";
+      }, 800); 
+    }, 5000); 
+  };
+});
