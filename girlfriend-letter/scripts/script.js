cea007746ce1dd8db2a191c683ef0f7ef42d4fea
KevinFerreira-BYUI <kferreira@byupathway.edu>
2025-06-05 07:38:52 -0300

    comitaaaa

--- girlfriend-letter/scripts/script.js
+++ girlfriend-letter/scripts/script.js
@@ -1,12 +1,35 @@
+
+AOS.init({
+  once: false,
+  duration: 1000,
+});
+
 let currentPage = 0;
-const pages = document.querySelectorAll('.page');
+const pages = document.querySelectorAll(".page");
 
-window.addEventListener('wheel', (e) => {
-  if (e.deltaY > 0) {
-    if (currentPage < pages.length - 1) currentPage++;
-  } else {
-    if (currentPage > 0) currentPage--;
+window.addEventListener("wheel", (e) => {
+  if (e.deltaY > 0 && currentPage < pages.length - 1) {
+    currentPage++;
+  } else if (e.deltaY < 0 && currentPage > 0) {
+    currentPage--;
   }
+  pages[currentPage].scrollIntoView({ behavior: "smooth" });
+});
 
-  pages[currentPage].scrollIntoView({ behavior: 'smooth' });
+let touchStartY = 0;
+
+window.addEventListener("touchstart", (e) => {
+  touchStartY = e.touches[0].clientY;
+});
+
+window.addEventListener("touchend", (e) => {
+  const touchEndY = e.changedTouches[0].clientY;
+  const deltaY = touchStartY - touchEndY;
+
+  if (deltaY > 50 && currentPage < pages.length - 1) {
+    currentPage++;
+  } else if (deltaY < -50 && currentPage > 0) {
+    currentPage--;
+  }
+  pages[currentPage].scrollIntoView({ behavior: "smooth" });
 });
