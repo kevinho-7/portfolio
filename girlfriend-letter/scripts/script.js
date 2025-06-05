59c4fc7f15c376ead0c3edcc03e6650bcefcc982
KevinFerreira-BYUI <kferreira@byupathway.edu>
2025-06-05 10:28:16 -0300

    estoy a ficar cansadenho

--- girlfriend-letter/scripts/script.js
+++ girlfriend-letter/scripts/script.js
@@ -1,35 +1,11 @@
+const elements = document.querySelectorAll('.fade-up-on-scroll');
 
-AOS.init({
-  once: false,
-  duration: 1000,
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
 });
 
-let currentPage = 0;
-const pages = document.querySelectorAll(".page");
-
-window.addEventListener("wheel", (e) => {
-  if (e.deltaY > 0 && currentPage < pages.length - 1) {
-    currentPage++;
-  } else if (e.deltaY < 0 && currentPage > 0) {
-    currentPage--;
-  }
-  pages[currentPage].scrollIntoView({ behavior: "smooth" });
-});
-
-let touchStartY = 0;
-
-window.addEventListener("touchstart", (e) => {
-  touchStartY = e.touches[0].clientY;
-});
-
-window.addEventListener("touchend", (e) => {
-  const touchEndY = e.changedTouches[0].clientY;
-  const deltaY = touchStartY - touchEndY;
-
-  if (deltaY > 50 && currentPage < pages.length - 1) {
-    currentPage++;
-  } else if (deltaY < -50 && currentPage > 0) {
-    currentPage--;
-  }
-  pages[currentPage].scrollIntoView({ behavior: "smooth" });
-});
+elements.forEach(el => observer.observe(el));
